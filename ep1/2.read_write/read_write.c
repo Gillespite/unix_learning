d1b17722ed10dd4f589d5a5be65026fcff2343df
Gillespite <lijipengqaz@icloud.com>
2023-09-30 02:50:29 +0800

    test

--- ep1/2.read_write/read_write.c
+++ ep1/2.read_write/read_write.c
@@ -5,12 +5,13 @@
 int main(){
     int n;
     char buf[BUFFSIZE];
+    //read的返回值是读到的字节数
+    //write的返回值是写入的字节数
     while ((n=read(STDIN_FILENO,buf,BUFFSIZE))>0){
         if(write(STDOUT_FILENO,buf,n)!=n){
             printf("write failed\n");
         }
     }
     printf("read failed\n");
-
     return 0;
 }
